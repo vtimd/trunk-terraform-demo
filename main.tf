9c44674e942abcb75bea567521e6682a7ef9d48e
Tim Davis <tim.davis.instinct@gmail.com>
2022-10-20 10:01:54 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   name                            = "${var.prefix}-vm"
   resource_group_name             = azurerm_resource_group.main.name
   location                        = azurerm_resource_group.main.location
-  size                            = var.size
+  size                            = "Standard_DS1_v1"
   admin_username                  = var.username
   admin_password                  = var.password
   disable_password_authentication = false
